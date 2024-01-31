9308963c4ca58792d522d289593d01f3dce1c87d
RayyanZaid <rayyanzaid0401@gmail.com>
2024-01-31 00:02:15 -0800

    Axios working

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+import axios from "axios";
 
-export default function App() {
+const API_URL = "http://192.168.4.45:5000/";
+
+const App = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(API_URL);
+      setData(response.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{data.name}</Text>
+      <Text>{data.major}</Text>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 70,
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
     justifyContent: "center",
+    alignItems: "center",
+    padding: 16,
+    backgroundColor: "#fff",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 16,
+  },
+  item: {
+    backgroundColor: "#f5f5f5",
+    padding: 10,
+    marginVertical: 8,
+    borderRadius: 8,
   },
 });
+
+export default App;
