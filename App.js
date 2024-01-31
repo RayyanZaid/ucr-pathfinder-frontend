6366841b5cf15c31c30445957bba501815196f89
RayyanZaid <rayyanzaid0401@gmail.com>
2024-01-31 00:14:21 -0800

    Separated axios testing

--- App.js
+++ App.js
@@ -1,53 +1,22 @@
-import React, { useState, useEffect } from "react";
-import { View, Text, StyleSheet, FlatList } from "react-native";
-import axios from "axios";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import TestForAxios from "./TestingInstallations/TestForAxios";
 
-const API_URL = "http://192.168.4.45:5000/";
-
-const App = () => {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  const fetchData = async () => {
-    try {
-      const response = await axios.get(API_URL);
-      setData(response.data);
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
-  };
-
+export default function App() {
   return (
     <View style={styles.container}>
-      <Text>{data.name}</Text>
-      <Text>{data.major}</Text>
+      <Text>Open up App.js to start working on your app!</Text>
+      {/* <TestForAxios /> */}
+      <StatusBar style="auto" />
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 70,
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    padding: 16,
     backgroundColor: "#fff",
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: "bold",
-    marginBottom: 16,
-  },
-  item: {
-    backgroundColor: "#f5f5f5",
-    padding: 10,
-    marginVertical: 8,
-    borderRadius: 8,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
-
-export default App;
