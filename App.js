b74d670f1bde1a13d71a827458c19c33f4159301
RayyanZaid <rayyanzaid0401@gmail.com>
2024-01-31 19:52:25 -0800

    final

--- App.js
+++ App.js
@@ -1,18 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
-import TestForAxios from "./TestingInstallations/TestForAxios";
-import TestForAsync from "./TestingInstallations/TestForAsync";
-import TestForMap from "./TestingInstallations/TestForMap";
-import TestForSecureStorage from "./TestingInstallations/TestForSecureStorage";
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      {/* <TestForAxios />
-      <TestForAsync />
-      <TestForMap /> */}
-      <TestForSecureStorage />
+
       <StatusBar style="auto" />
     </View>
   );
