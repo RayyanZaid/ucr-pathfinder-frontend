ab735e44dc91fcff2eeb530efc6b9aace8335627
RayyanZaid <rayyanzaid0401@gmail.com>
2024-02-27 01:22:36 -0800

    made it more modular

--- screens/Landing.js
+++ screens/Landing.js
@@ -1,36 +1,124 @@
 import {
   ActivityIndicator,
   Button,
+  Dimensions,
   StyleSheet,
   Text,
   View,
 } from "react-native";
 import text_styles from "../styles/text_styles";
-import MapWithPath from "../components/Map";
+import MapWithPath from "../components/MapComponents/PreviewStage";
 import EachCourse from "../components/CourseComponents/EachCourse";
 import { useEffect, useState } from "react";
 import api from "../api";
 import button_styles from "../styles/button_styles";
+import * as Location from "expo-location";
+import NavigationStage from "../components/MapComponents/NavigationStage";
+
+const screenHeight = Dimensions.get("window").height;
+
+var ucrRegion = {
+  latitude: 33.9737,
+  longitude: -117.3281,
+  latitudeDelta: 0.009,
+  longitudeDelta: 0.009,
+};
+
 export default function LandingScreen() {
-  // replace with local storage schedule later
+  // State Variables
+
   const [nextClass, setNextClass] = useState(null);
   const [isInNavigation, setIsInNavigation] = useState(false);
 
-  // This gets the student's next class. Right now it's just a mock class. Need to implement in the backend
+  // Location State Variables
+
+  const [location, setLocation] = useState(null);
+  const [latitude, setLatitude] = useState(null);
+  const [longitude, setLongitude] = useState(null);
+  const [altitude, setAltitude] = useState(null);
+
+  // Navigation State Variables
+
+  const [nodes, setNodes] = useState(null);
+  const [edges, setEdges] = useState(null);
+  const [minutesNeeded, setMinutesNeeded] = useState(null);
+  const [distance, setDistance] = useState(null);
+
+  // Fetching user's current location
   useEffect(() => {
-    const uid = "rayyanzaid0401@gmail.com";
-    api
-      .get("/getNextClass", { params: { uid } })
-      .then((response) => {
-        setNextClass(response.data["nextClass"]);
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-      });
+    console.log("Getting User Location");
+    const fetchLocation = async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        console.error("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+      setLatitude(location.coords.latitude);
+      setLongitude(location.coords.longitude);
+      setAltitude(location.coords.altitude);
+    };
+
+    fetchLocation();
+
+    const locationFetchInterval = setInterval(fetchLocation, 1000);
+
+    return () => clearInterval(locationFetchInterval);
   }, []);
+
+  // Fetching the next class data
+  useEffect(() => {
+    const fetchNextClass = async () => {
+      const uid = "rayyanzaid0401@gmail.com";
+      try {
+        const response = await api.get("/getNextClass", { params: { uid } });
+        setNextClass(response.data["nextClass"]);
+      } catch (error) {
+        console.error("Error fetching next class data:", error);
+      }
+    };
+
+    fetchNextClass();
+  }, []);
+
+  useEffect(() => {
+    // Directly check if all required data is present
+    if (
+      nextClass &&
+      latitude != null &&
+      longitude != null &&
+      altitude != null
+    ) {
+      getNavigationData();
+    }
+  }, [nextClass, latitude, longitude, altitude]);
 
   const toggleNavigation = () => {
     setIsInNavigation(!isInNavigation);
+  };
+
+  const getNavigationData = async () => {
+    console.log("Getting Navigation data from backend");
+    const uid = "rayyanzaid0401@gmail.com";
+    let classBuildingName = nextClass["locationInfo"]["buildingName"];
+    try {
+      api
+        .get("/getShortestPath", {
+          params: { uid, latitude, longitude, altitude, classBuildingName },
+        })
+        .then((response) => {
+          if (response) {
+            setNodes(response.data["nodes"]);
+            setEdges(response.data["edges"]);
+            setMinutesNeeded(Math.ceil(response.data["totalTime"]));
+            setDistance(Math.ceil(response.data["totalLength"]));
+          }
+        });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   if (!nextClass) {
@@ -49,7 +137,10 @@ export default function LandingScreen() {
               Path to your {nextClass["courseNumber"]} class
             </Text>
             <MapWithPath
-              classBuildingName={nextClass["locationInfo"]["buildingName"]}
+              nodes={nodes}
+              edges={edges}
+              minutesNeeded={minutesNeeded}
+              distance={distance}
             />
 
             <Button
@@ -61,7 +152,7 @@ export default function LandingScreen() {
           </>
         ) : (
           <>
-            <Text style={text_styles.titleText}>In Navigation</Text>
+            <NavigationStage />
             <Button
               onPress={toggleNavigation}
               title="Cancel Navigation"
@@ -80,5 +171,6 @@ const styles = StyleSheet.create({
 
     alignItems: "center",
     justifyContent: "center",
+    marginVertical: screenHeight * 0.05,
   },
 });
