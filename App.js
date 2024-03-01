4e7e2f7d68adb20e39e21723ac2ed1e0707831c4
RayyanZaid <rayyanzaid0401@gmail.com>
2024-03-01 01:16:35 -0800

    added nav bar

--- App.js
+++ App.js
@@ -1,24 +1,36 @@
+import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import ScheduleScreen from "./screens/Schedule";
-
-import { useFonts } from "expo-font";
 import LandingScreen from "./screens/Landing";
+import { useFonts } from "expo-font";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     Gabarito: require("./assets/fonts/Gabarito-VariableFont_wght.ttf"),
   });
 
-  if (fontsLoaded) {
-    return (
-      <View style={styles.container}>
-        {/* <ScheduleScreen /> */}
-        <LandingScreen />
-        <StatusBar style="auto" />
-      </View>
-    );
+  if (!fontsLoaded) {
+    return <View />;
   }
+
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={{
+          headerShown: false, // This line hides the header
+        }}
+      >
+        <Tab.Screen name="Landing" component={LandingScreen} />
+        <Tab.Screen name="Schedule" component={ScheduleScreen} />
+      </Tab.Navigator>
+      <StatusBar style="auto" />
+    </NavigationContainer>
+  );
 }
 
 const styles = StyleSheet.create({
