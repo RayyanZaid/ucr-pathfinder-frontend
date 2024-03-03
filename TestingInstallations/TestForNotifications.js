502c7112fec5be149ad4aca0a637d7788fd566a7
RayyanZaid <rayyanzaid0401@gmail.com>
2024-03-02 16:35:33 -0800

    using expo notifications

--- TestingInstallations/TestForNotifications.js
+++ TestingInstallations/TestForNotifications.js
@@ -1,35 +1,74 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { StyleSheet, View, Text, Button } from "react-native";
-import { Notifications } from "expo";
+import * as Notifications from "expo-notifications";
+import Constants from "expo-constants";
 
-const getDeviceToken = async () => {
-  const { status: existingStatus } = await Notifications.getPermissionsAsync();
-  let finalStatus = existingStatus;
-
-  if (existingStatus !== "granted") {
-    const { status } = await Notifications.requestPermissionsAsync();
-    finalStatus = status;
-  }
-
-  if (finalStatus !== "granted") {
-    console.log("Permission to access notifications not granted");
-    return;
-  }
-
-  const tokenObj = await Notifications.getExpoPushTokenAsync();
-  const token = tokenObj.data;
-  console.log("Expo Push Token:", token);
-  // Send this token to your Flask backend via an API call
-};
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 
 const TestForNotifications = () => {
+  useEffect(() => {
+    registerForPushNotificationsAsync();
+  }, []);
+
+  const sendLocalNotification = async () => {
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: "Look at that notification",
+        body: "I'm so proud of myself!",
+      },
+      trigger: null, // means send it immediately
+    });
+  };
+
   return (
     <View style={styles.container}>
       <Text>Hi</Text>
-      <Button title="Push to print token" onPress={getDeviceToken} />
+      <Button title="Push to send notif" onPress={sendLocalNotification} />
     </View>
   );
 };
+
+async function registerForPushNotificationsAsync() {
+  let token;
+  if (Constants.isDevice) {
+    const { status: existingStatus } =
+      await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== "granted") {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== "granted") {
+      alert("Failed to get push token for push notification!");
+      return;
+    }
+    token = (
+      await Notifications.getExpoPushTokenAsync({
+        projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
+      })
+    ).data;
+    console.log(token);
+  } else {
+    alert("Must use physical device for Push Notifications");
+  }
+
+  if (Platform.OS === "android") {
+    Notifications.setNotificationChannelAsync("default", {
+      name: "default",
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: "#FF231F7C",
+    });
+  }
+
+  return token;
+}
 
 const styles = StyleSheet.create({
   container: {
