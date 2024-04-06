89a79ee92a2a557552e4a6a8099d6da6688248c4
RayyanZaid <rayyanzaid0401@gmail.com>
2024-04-06 00:50:42 -0700

    changed api for aws

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `http://${process.env.EXPO_PUBLIC_LOCAL_IP_ADDRESS}:5000`,
+  baseURL: `http://${process.env.EXPO_PUBLIC_LOCAL_IP_ADDRESS}`,
   headers: {
     headers: {
       "Content-Type": "multipart/form-data",
